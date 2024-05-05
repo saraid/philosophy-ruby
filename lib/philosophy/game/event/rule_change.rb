fea5fc23f7504e6efbb97146b84b8c701fb5daed
Michael Chui <saraid216@gmail.com>
2024-05-04 22:02:05 -0700

    how long has this been namespaced wrong

--- lib/philosophy/game/event/rule_change.rb
+++ lib/philosophy/game/event/rule_change.rb
@@ -1,35 +1,33 @@
 module Philosophy
   class Game
-    class Event
-      class RuleChange < Event
-        REGEXES = {
-          rule: "(?<rule>join|leave)",
-          variable: "(?<variable>permitted|where|effect)",
-          value: "(?<value>\\w+)",
-        }
-        NOTATION_REGEX = REGEXES
-          .values_at(:rule, :variable, :value)
-          .prepend(:rule)
-          .join(':')
-          .then { Regexp.new _1 }
+    class RuleChange < Event
+      REGEXES = {
+        rule: "(?<rule>join|leave)",
+        variable: "(?<variable>permitted|where|effect)",
+        value: "(?<value>\\w+)",
+      }
+      NOTATION_REGEX = REGEXES
+        .values_at(:rule, :variable, :value)
+        .prepend(:rule)
+        .join(':')
+        .then { Regexp.new _1 }
 
-        def self.from_notation(notation)
-          md = notation.match(NOTATION_REGEX)
-          new(
-            rule: md[:rule].to_sym,
-            variable: md[:variable].to_sym,
-            value: md[:value].to_sym
-          )
-        end
-
-        def initialize(rule:, variable:, value:)
-          @rule, @variable, @value = rule, variable, value
-        end
-        attr_reader :rule, :variable, :value
-
-        def execute(game) = game.rule_change(rule: rule, variable: variable, value: value)
-        def notation = [:rule, rule, variable, value].join(':')
+      def self.from_notation(notation)
+        md = notation.match(NOTATION_REGEX)
+        new(
+          rule: md[:rule].to_sym,
+          variable: md[:variable].to_sym,
+          value: md[:value].to_sym
+        )
       end
+
+      def initialize(rule:, variable:, value:)
+        @rule, @variable, @value = rule, variable, value
+      end
+      attr_reader :rule, :variable, :value
+
+      def execute(game) = game.rule_change(rule: rule, variable: variable, value: value)
+      def notation = [:rule, rule, variable, value].join(':')
     end
   end
 end
