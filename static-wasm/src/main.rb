a16b83c5e8f7254c557206823cb5d58db0929cd3
Michael Chui <saraid216@gmail.com>
2026-01-10 12:32:00 -0800

    src2 -> src

--- static-wasm/src/main.rb
+++ static-wasm/src/main.rb
@@ -22,135 +22,46 @@ end
 
 require_relative '../lib/philosophy'
 require_relative '../lib/philosophy/shims/svg'
-require_relative './src/move'
-require_relative './src/player'
-require_relative './src/tests'
+require_relative './src2/ux'
+
+class JS::Object
+  def then(&) = yield(self)
+  def tap(&)
+    yield(self)
+    self
+  end
+end
+
+class Philosophy::Game
+  def self.current = @current_game
+  def self.set_current!(g = new) = @current_game = g
+end
+
+module Ux
+  module State
+    VALUES = %i[
+      choose_tile_for_move
+      choose_space_for_move
+      choose_space_for_choice
+      choose_direction_for_move
+      choose_direction_for_choice
+    ].each { |s| define_singleton_method(s) { s } }
+    #.map { [_1, _1] }.to_h
+
+    def self.current = @current
+    def self.set_to(state)
+      puts "Setting state to #{state}"
+      @current = state
+    end
+    set_to choose_tile_for_move
+  end
+end
 
 def document = JS.global[:document]
+def current_game = Philosophy::Game.current
+Philosophy::Game.set_current!
 
-STATES = %i[
-  choose_tile_for_move
-  choose_space_for_move
-  choose_space_for_choice
-  choose_direction_for_move
-  choose_direction_for_choice
-].map { [_1, _1] }.to_h
+Ux.setup
 
-def current_state = @current_state
-def set_current_state!(state)
-  @current_state = STATES.fetch(state)
-  puts "Current state: #{current_state}"
-end
-set_current_state!(:choose_tile_for_move)
-
-def update_pgn = JS.global[:document].querySelector('#pgn')[:innerHTML] = current_game.to_pgn
-
-def render_game(game = current_game)
-  # clean the state
-  JS.global[:document].querySelector('#error')[:innerHTML] = ""
-  game.board.each do
-    JS.global[:document].querySelector("#space-#{_1.name}")[:innerHTML] = _1.name.to_s
-  end
-  overlay = JS.global[:document].querySelector("#operations")
-  JS.global[:document].querySelector("#operations")[:childNodes].forEach do |child|
-    overlay.removeChild(child)
-  end
-
-  # render new state
-  render_all_players
-  update_pgn
-  game.board.each.select(&:occupied?).each do
-    space = JS.global[:document].querySelector("#space-#{_1.name}")
-    space[:innerHTML] = _1.tile.notation
-    space[:title] = <<~TEXT
-      Player: #{_1.tile.owner.color.name}
-      Tile: #{_1.to_s}
-      Direction: #{_1.tile.target}
-    TEXT
-  end
-
-  game.last_board_operations.each do |operation|
-    case operation
-    when Philosophy::ActivationContext::Operation::Place
-      location = operation.location.name
-      current_html = JS.global[:document].querySelector("#space-#{location}")[:innerHTML]
-      JS.global[:document].querySelector("#space-#{location}")[:innerHTML] = "#{current_html}<br/>#{operation.to_svg}"
-    when Philosophy::ActivationContext::Operation::Rotate
-      location = operation.target_location.name
-      current_html = JS.global[:document].querySelector("#space-#{location}")[:innerHTML]
-      JS.global[:document].querySelector("#space-#{location}")[:innerHTML] = "#{current_html}<br/>#{operation.to_svg}"
-    when Philosophy::ActivationContext::Operation::Move
-      from_location = operation.from_location.coordinate
-      start_column = from_location.col + 1
-      end_column =
-        case operation.impact_direction.value
-        when :east, :ne, :se then start_column + operation.impact_distance
-        when :west, :nw, :sw then start_column - operation.impact_distance
-        else start_column
-        end
-      start_row = from_location.row + 1
-      end_row =
-        case operation.impact_direction.value
-        when :north, :nw, :ne then start_row - operation.impact_distance
-        when :south, :sw, :se then start_row + operation.impact_distance
-        else start_row
-        end
-      grid_position =
-        [ [start_column, end_column].sort.then { "grid-column-start:#{_1};grid-column-end:span #{_2};" },
-          [start_row, end_row].sort.then { "grid-row-start:#{_1};grid-row-end:span #{_2};" },
-        ].join#.tap { puts "v2: #{_1}" }
-
-      div = JS.global[:document].createElement("div")
-      div[:class] = "operation-move"
-      div[:style] = [
-        grid_position,
-        "z-index:2;"
-      ].join
-      div[:innerHTML] = operation.to_svg
-      JS.global[:document].querySelector("#operations").appendChild(div)
-    else
-      puts "unknown operation: #{operation.inspect}"
-    end
-  end
-  nil # return nil to stop a mystery error about JS::Object#call receiving a non-JS::Object on arg 2.
-end
-
-def current_game = @current_game
-@current_game = Philosophy::Game.new
-
-def input  = document.querySelector('#input input')
-def submit_movetext
-  begin
-    current_game << input[:value].to_s
-    render_game
-  rescue Philosophy::Game::Placement::InvalidFirstMove
-    JS.global[:document].querySelector('#error')[:innerHTML] = "You may not play C5 first."
-  rescue Philosophy::Game::Placement::LocationOutsidePlacementSpace
-    JS.global[:document].querySelector('#error')[:innerHTML] = "You must play within the central 9 spaces."
-  rescue Philosophy::Game::Placement::InvalidTileType
-    JS.global[:document].querySelector('#error')[:innerHTML] = "Unrecognized tile type selected."
-  rescue Philosophy::Game::Placement::UnavailableTile
-    JS.global[:document].querySelector('#error')[:innerHTML] = "You've already played that tile."
-  rescue Philosophy::Game::Placement::InvalidLocation
-    JS.global[:document].querySelector('#error')[:innerHTML] = "This location does not exist."
-  rescue Philosophy::Game::Placement::CannotPlaceAtopExistingTile
-    JS.global[:document].querySelector('#error')[:innerHTML] = "A tile is already in this space. Cannot place."
-  rescue Philosophy::Game::Placement::CannotOrientInTargetDirection
-    JS.global[:document].querySelector('#error')[:innerHTML] = "This tile cannot be oriented in that direction."
-  rescue Philosophy::Game::Placement::IncorrectPlayer
-    JS.global[:document].querySelector('#error')[:innerHTML] = "It is not this player's turn."
-  rescue Philosophy::Game::Choice::Error
-    JS.global[:document].querySelector('#error')[:innerHTML] = "Not a valid choice."
-  end
-end
-document.querySelector('#input button').call(:addEventListener, "click") { |event| submit_movetext }
-input.addEventListener("keypress") do |event|
-  case event[:key].to_s
-  when 'Enter'
-    submit_movetext
-  else
-    #puts event[:key].inspect
-  end
-end
-
-render_game
+Ux::Player.render_available
+nil
